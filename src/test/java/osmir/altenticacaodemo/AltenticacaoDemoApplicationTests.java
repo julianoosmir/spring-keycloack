b3ddab5f16045a5a285e433f3e3967890bf9f0da
juliano <julianoosmir@outlook.com>
2022-10-15 22:21:05 -0400

    first commit

---
+++ src/test/java/osmir/altenticacaodemo/AltenticacaoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package osmir.altenticacaodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltenticacaoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
